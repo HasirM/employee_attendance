b464fea0b4f9eb5b19087910347fa67dbaf0b79b
HasirM <83203418+HasirM@users.noreply.github.com>
2024-03-29 16:39:47 +0530

    commit 2

--- database/eas_db.sql
+++ database/eas_db.sql
@@ -1,15 +1,21 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.3
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Jun 09, 2022 at 08:42 AM
--- Server version: 10.4.24-MariaDB
--- PHP Version: 8.1.5
+-- Host: 127.0.0.1:3306
+-- Generation Time: Mar 29, 2024 at 11:09 AM
+-- Server version: 8.0.31
+-- PHP Version: 8.0.26
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Database: `eas_db`
@@ -21,47 +27,36 @@ SET time_zone = "+00:00";
 -- Table structure for table `attendance`
 --
 
-CREATE TABLE `attendance` (
-  `id` int(11) NOT NULL,
+DROP TABLE IF EXISTS `attendance`;
+CREATE TABLE IF NOT EXISTS `attendance` (
+  `id` int NOT NULL AUTO_INCREMENT,
   `username` char(6) NOT NULL,
   `employee_id` int(3) UNSIGNED ZEROFILL NOT NULL,
   `department_id` char(3) NOT NULL,
-  `shift_id` int(1) NOT NULL,
-  `location_id` int(1) NOT NULL,
-  `in_time` int(11) NOT NULL,
+  `shift_id` int NOT NULL,
+  `location_id` int NOT NULL,
+  `in_time` int NOT NULL,
   `notes` varchar(120) NOT NULL,
-  `image` varchar(50) NOT NULL,
+  `image` varchar(50) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
   `lack_of` varchar(11) NOT NULL,
   `in_status` varchar(15) NOT NULL,
-  `out_time` int(11) NOT NULL,
-  `out_status` varchar(15) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `out_time` int NOT NULL,
+  `out_status` varchar(15) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `username` (`username`),
+  KEY `employee_id` (`employee_id`),
+  KEY `department_id` (`department_id`),
+  KEY `shift_id` (`shift_id`),
+  KEY `location_id` (`location_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=120 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `attendance`
 --
 
 INSERT INTO `attendance` (`id`, `username`, `employee_id`, `department_id`, `shift_id`, `location_id`, `in_time`, `notes`, `image`, `lack_of`, `in_status`, `out_time`, `out_status`) VALUES
-(45, 'ADM011', 011, 'ADM', 1, 1, 1589178316, 'sdf', 'item-200511-8f5d7be1a1.jpg', 'None', 'Late', 1589178477, 'Early'),
-(48, 'ADM011', 011, 'ADM', 1, 1, 1589381121, '', 'item-200513-ad6953a07e.jpg', 'Notes', 'Late', 1589381127, 'Over Time'),
-(49, 'PCD010', 010, 'PCD', 2, 1, 1589384432, 'asdasd', '', 'None,image', 'Late', 1589384514, 'Over Time'),
-(50, 'ADM011', 011, 'ADM', 1, 1, 1589391038, '', '', 'Notes,image', 'On Time', 1589391056, 'Early'),
-(51, 'PCD010', 010, 'PCD', 3, 1, 1622553388, 'testing', 'item-210601-3946bb00df.png', 'None', 'Late', 1622553470, 'Over Time'),
-(52, 'PCD010', 010, 'PCD', 3, 2, 1631893356, 'none', '', 'None,image', 'Late', 1631893413, 'Over Time'),
-(53, 'STD026', 026, 'STD', 1, 1, 1631894335, 'none', '', 'None,image', 'Late', 1631894403, 'Over Time'),
-(54, 'ADM011', 011, 'ADM', 1, 2, 1631894692, 'demo', '', 'None,image', 'Late', 1631894696, 'Over Time'),
-(55, 'QCD027', 027, 'QCD', 6, 2, 1631499386, 'none..', '', 'None,image', 'Late', 1631529057, 'Early'),
-(56, 'QCD027', 027, 'QCD', 6, 2, 1631583036, 'none', '', 'None,image', 'Late', 1631611849, 'Early'),
-(58, 'QCD027', 027, 'QCD', 6, 1, 1631733350, 'none', '', 'None,image', 'Late', 1631797356, 'Early'),
-(59, 'QCD027', 027, 'QCD', 6, 4, 1631863331, 'none', '', 'None,image', 'Late', 1631896539, 'Early'),
-(60, 'QCD027', 027, 'QCD', 6, 1, 1631214919, 'none', '', 'None,image', 'Late', 1631250936, 'Over Time'),
-(61, 'STD026', 026, 'STD', 1, 2, 1631493955, 'none', '', 'None,image', 'On Time', 1631523613, 'Over Time'),
-(62, 'ADM011', 011, 'ADM', 1, 1, 1631584873, 'none', '', 'None,image', 'Late', 1631621603, 'Over Time'),
-(63, 'QCD027', 027, 'QCD', 6, 2, 1632109417, 'this is a demo note!', '', 'None,image', 'Late', 1632109437, 'Early'),
-(64, 'ACD002', 002, 'ACD', 2, 3, 1632109840, 'demo demo', '', 'None,image', 'On Time', 1632109845, 'Early'),
-(65, 'STD026', 026, 'STD', 1, 2, 1632109903, 'test', '', 'None,image', 'Late', 1632109905, 'Early'),
-(66, 'ITD028', 028, 'ITD', 7, 7, 1654738570, 'Sample Time-In', 'item-220609-6dc7d7e8fe.png', 'None', 'On Time', 1654738629, 'Early'),
-(71, 'HRD030', 030, 'HRD', 7, 7, 1654756719, 'Sample Note only', 'item-220609-496d1ceffe.png', 'None', 'Late', 0, '');
+(118, 'HRD032', 032, 'HRD', 10, 1, 1711624071, 'Omni Bus stand, Melapudur main road, Ward 53, Trichy Zone IV, Trichy Corporation Limits, Tiruchirappalli, Tiruchchir?ppa', 'attendance-240328-15718cc253.jpeg', 'None', 'Late', 1711624077, 'Early'),
+(119, 'DSN033', 033, 'DSN', 10, 1, 1711707696, 'live location here', 'attendance-240329-ec74d4686e.jpeg', 'None', 'Late', 1711707761, 'Early');
 
 -- --------------------------------------------------------
 
@@ -69,9 +64,11 @@ INSERT INTO `attendance` (`id`, `username`, `employee_id`, `department_id`, `shi
 -- Table structure for table `department`
 --
 
-CREATE TABLE `department` (
+DROP TABLE IF EXISTS `department`;
+CREATE TABLE IF NOT EXISTS `department` (
   `id` char(3) NOT NULL,
-  `name` varchar(50) NOT NULL
+  `name` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -79,15 +76,8 @@ CREATE TABLE `department` (
 --
 
 INSERT INTO `department` (`id`, `name`) VALUES
-('ACD', 'Accounting Department'),
-('ADM', 'Admin Department'),
-('HRD', 'Human Resource Department'),
-('ITD', 'Information Technology Department - Updated'),
-('PCD', 'Production Controller Department'),
-('PLD', 'Planner Department'),
-('QCD', 'Quality Control Department'),
-('SCD', 'Security Department'),
-('STD', 'Store Department');
+('DSN', 'DESIGNING'),
+('HRD', 'Human Resource Department');
 
 -- --------------------------------------------------------
 
@@ -95,26 +85,28 @@ INSERT INTO `department` (`id`, `name`) VALUES
 -- Table structure for table `employee`
 --
 
-CREATE TABLE `employee` (
-  `id` int(3) UNSIGNED ZEROFILL NOT NULL,
+DROP TABLE IF EXISTS `employee`;
+CREATE TABLE IF NOT EXISTS `employee` (
+  `id` int(3) UNSIGNED ZEROFILL NOT NULL AUTO_INCREMENT,
   `name` varchar(50) NOT NULL,
   `email` varchar(128) NOT NULL,
   `gender` char(1) NOT NULL,
   `image` varchar(128) NOT NULL,
   `birth_date` date NOT NULL,
   `hire_date` date NOT NULL,
-  `shift_id` int(1) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `shift_id` int NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `shift_id_fk_e` (`shift_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=34 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `employee`
 --
 
 INSERT INTO `employee` (`id`, `name`, `email`, `gender`, `image`, `birth_date`, `hire_date`, `shift_id`) VALUES
-(001, 'John Smith', 'jsmith@mail.com', 'M', 'default.png', '1996-07-15', '2020-03-05', 7),
-(025, 'Admin ', 'admin@admin.com', 'M', 'default.png', '0000-00-00', '0000-00-00', 0),
-(028, 'Mark Cooper', 'mcooper@mail.com', 'M', 'item-220609-64856edd56.png', '1997-06-23', '2021-04-20', 7),
-(030, 'Claire Blake', 'cblake@mail.com', 'F', 'item-220609-97e2f7f0fe.png', '1997-10-14', '2020-03-05', 7);
+(025, 'Admin ', 'admin@admin.com', 'M', 'prizytech_logo.png', '0000-00-00', '0000-00-00', 0),
+(032, 'Shamil Sha', 'shamilsha1230@gmail.com', 'M', 'item-240328-88e24c486c.jpg', '2002-01-01', '2024-03-28', 10),
+(033, 'Bhanu', 'test@gmail.com', 'M', 'item-240329-59b804f2ae.jpg', '2001-01-16', '2024-02-15', 10);
 
 -- --------------------------------------------------------
 
@@ -122,33 +114,23 @@ INSERT INTO `employee` (`id`, `name`, `email`, `gender`, `image`, `birth_date`, 
 -- Table structure for table `employee_department`
 --
 
-CREATE TABLE `employee_department` (
-  `id` int(3) NOT NULL,
+DROP TABLE IF EXISTS `employee_department`;
+CREATE TABLE IF NOT EXISTS `employee_department` (
+  `id` int NOT NULL AUTO_INCREMENT,
   `employee_id` int(3) UNSIGNED ZEROFILL NOT NULL,
-  `department_id` char(3) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `department_id` char(3) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `employee_department_ibfk_1` (`employee_id`),
+  KEY `employee_department_ibfk_2` (`department_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=34 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `employee_department`
 --
 
 INSERT INTO `employee_department` (`id`, `employee_id`, `department_id`) VALUES
-(1, 001, 'HRD'),
-(2, 002, 'ACD'),
-(3, 003, 'QCD'),
-(4, 004, 'SCD'),
-(5, 005, 'STD'),
-(6, 006, 'ACD'),
-(7, 007, 'PLD'),
-(8, 008, 'STD'),
-(9, 009, 'STD'),
-(10, 010, 'PCD'),
-(21, 011, 'ADM'),
-(25, 024, 'HRD'),
-(26, 026, 'STD'),
-(27, 027, 'QCD'),
-(28, 028, 'ITD'),
-(30, 030, 'HRD');
+(32, 032, 'HRD'),
+(33, 033, 'DSN');
 
 -- --------------------------------------------------------
 
@@ -156,10 +138,12 @@ INSERT INTO `employee_department` (`id`, `employee_id`, `department_id`) VALUES
 -- Table structure for table `location`
 --
 
-CREATE TABLE `location` (
-  `id` int(1) NOT NULL,
-  `name` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+DROP TABLE IF EXISTS `location`;
+CREATE TABLE IF NOT EXISTS `location` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `location`
@@ -167,11 +151,7 @@ CREATE TABLE `location` (
 
 INSERT INTO `location` (`id`, `name`) VALUES
 (1, 'Home'),
-(2, 'Office'),
-(3, 'Store'),
-(4, 'Site'),
-(6, 'Field'),
-(7, 'XYZ Client Office');
+(4, 'Site');
 
 -- --------------------------------------------------------
 
@@ -179,25 +159,20 @@ INSERT INTO `location` (`id`, `name`) VALUES
 -- Table structure for table `shift`
 --
 
-CREATE TABLE `shift` (
-  `id` int(1) NOT NULL,
+DROP TABLE IF EXISTS `shift`;
+CREATE TABLE IF NOT EXISTS `shift` (
+  `id` int NOT NULL AUTO_INCREMENT,
   `start` time NOT NULL,
-  `end` time NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `end` time NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `shift`
 --
 
 INSERT INTO `shift` (`id`, `start`, `end`) VALUES
-(1, '08:00:00', '16:00:00'),
-(2, '13:00:00', '21:00:00'),
-(3, '18:00:00', '02:00:00'),
-(4, '03:15:02', '02:05:05'),
-(5, '07:00:00', '18:25:00'),
-(6, '01:00:00', '12:00:00'),
-(7, '09:30:00', '18:30:00'),
-(8, '23:23:23', '23:23:23');
+(10, '10:00:00', '20:00:00');
 
 -- --------------------------------------------------------
 
@@ -205,11 +180,15 @@ INSERT INTO `shift` (`id`, `start`, `end`) VALUES
 -- Table structure for table `users`
 --
 
-CREATE TABLE `users` (
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE IF NOT EXISTS `users` (
   `username` char(6) NOT NULL,
   `password` varchar(128) NOT NULL,
   `employee_id` int(3) UNSIGNED ZEROFILL NOT NULL,
-  `role_id` int(1) NOT NULL
+  `role_id` int NOT NULL,
+  PRIMARY KEY (`username`),
+  KEY `employee_id` (`employee_id`),
+  KEY `role_id` (`role_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -217,17 +196,9 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`username`, `password`, `employee_id`, `role_id`) VALUES
-('ACD002', '$2y$10$5nv5ehyMVdljfKJ6izsOqOimsbv.cbzU.XLB9ji9zbA.eICdSrNvO', 002, 2),
-('ADM011', '$2y$10$BKpQcs4XKavCcYdFWujzx.Xqb7r9eNkDrOYss2VNXrMJUUpm1agUC', 011, 2),
-('admin', '$2y$10$tG8hnh7w.yiJL7fjlRCmkuHBwi0QI6DHFXmc1s6X3cAFuw5pRfO/O', 025, 1),
-('HRD001', '$2y$10$7rLSvRVyTQORapkDOqmkhetjF6H9lJHngr4hJMSM2lHObJbW5EQh6', 001, 2),
-('HRD030', '$2y$10$nGeEuKaPiQ45O6Yvt3BxGuxY.BN.IfMsxP4nWMkM.yXxQkTRyRfDa', 030, 2),
-('ITD028', '$2y$10$vQQ80W50S/rsXThp2YkQ5uH3XKej4bZE68L7Cfz9.MVcQ0EiJo.Hy', 028, 2),
-('PCD010', '$2y$10$BKpQcs4XKavCcYdFWujzx.Xqb7r9eNkDrOYss2VNXrMJUUpm1agUC', 010, 2),
-('QCD027', '$2y$10$peALJo.JKZyD6uMBd41UfuHGQSJe7ExOfDhPITvDbSRRXeWUGY9xy', 027, 2),
-('STD005', '$2y$10$hr35h1fIySFYCSRVL2jRD.RuYa9WtJCEJkkqvQfPboYK7VwURpLim', 005, 2),
-('STD008', '$2y$10$8PGnFaiZPYtcIGrwzMmVZuNKbUb/A88f0NZOA9QVgHaUIJ6ddg.Si', 008, 2),
-('STD026', '$2y$10$8WNMvEEgNPWyRuSeeLDE1uXwnBkYNJE/heLT1zWbsUfYb/wKFyYIy', 026, 2);
+('admin', '$2y$10$1xjbpb5fmmkSMTZnKhz0vugDvkVsKXEEfffgn/KDp6ghn/AUmsLYy', 025, 1),
+('DSN033', '$2y$10$Tqe6G0n7qg2ySWZqtIEKQONFki19QLtBqUH38T0SgY2T0EAS4wOYm', 033, 2),
+('HRD032', '$2y$10$4xylwnGhnvEoZX5LELVSJ.y4zCsnUmIr6BXqYwwuxeQ7OkoAK0X9O', 032, 2);
 
 -- --------------------------------------------------------
 
@@ -235,11 +206,15 @@ INSERT INTO `users` (`username`, `password`, `employee_id`, `role_id`) VALUES
 -- Table structure for table `user_access`
 --
 
-CREATE TABLE `user_access` (
-  `id` int(2) NOT NULL,
-  `role_id` int(1) NOT NULL,
-  `menu_id` int(2) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+DROP TABLE IF EXISTS `user_access`;
+CREATE TABLE IF NOT EXISTS `user_access` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `role_id` int NOT NULL,
+  `menu_id` int NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `menu_id` (`menu_id`),
+  KEY `role_id` (`role_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `user_access`
@@ -258,10 +233,12 @@ INSERT INTO `user_access` (`id`, `role_id`, `menu_id`) VALUES
 -- Table structure for table `user_menu`
 --
 
-CREATE TABLE `user_menu` (
-  `id` int(2) NOT NULL,
-  `menu` varchar(20) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+DROP TABLE IF EXISTS `user_menu`;
+CREATE TABLE IF NOT EXISTS `user_menu` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `menu` varchar(20) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `user_menu`
@@ -280,10 +257,12 @@ INSERT INTO `user_menu` (`id`, `menu`) VALUES
 -- Table structure for table `user_role`
 --
 
-CREATE TABLE `user_role` (
-  `id` int(1) NOT NULL,
-  `name` varchar(20) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+DROP TABLE IF EXISTS `user_role`;
+CREATE TABLE IF NOT EXISTS `user_role` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(20) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `user_role`
@@ -299,14 +278,17 @@ INSERT INTO `user_role` (`id`, `name`) VALUES
 -- Table structure for table `user_submenu`
 --
 
-CREATE TABLE `user_submenu` (
-  `id` int(2) NOT NULL,
-  `menu_id` int(2) NOT NULL,
+DROP TABLE IF EXISTS `user_submenu`;
+CREATE TABLE IF NOT EXISTS `user_submenu` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `menu_id` int NOT NULL,
   `title` varchar(20) NOT NULL,
   `url` varchar(50) NOT NULL,
   `icon` varchar(50) NOT NULL,
-  `is_active` int(1) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `is_active` int NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `menu_id` (`menu_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `user_submenu`
@@ -326,147 +308,6 @@ INSERT INTO `user_submenu` (`id`, `menu_id`, `title`, `url`, `icon`, `is_active`
 (12, 3, 'History', 'attendance/history', 'fas fa-fw fa-th-list', 1);
 
 --
--- Indexes for dumped tables
---
-
---
--- Indexes for table `attendance`
---
-ALTER TABLE `attendance`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `username` (`username`),
-  ADD KEY `employee_id` (`employee_id`),
-  ADD KEY `department_id` (`department_id`),
-  ADD KEY `shift_id` (`shift_id`),
-  ADD KEY `location_id` (`location_id`);
-
---
--- Indexes for table `department`
---
-ALTER TABLE `department`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `employee`
---
-ALTER TABLE `employee`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `shift_id_fk_e` (`shift_id`);
-
---
--- Indexes for table `employee_department`
---
-ALTER TABLE `employee_department`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `employee_department_ibfk_1` (`employee_id`),
-  ADD KEY `employee_department_ibfk_2` (`department_id`);
-
---
--- Indexes for table `location`
---
-ALTER TABLE `location`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `shift`
---
-ALTER TABLE `shift`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `users`
---
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`username`),
-  ADD KEY `employee_id` (`employee_id`),
-  ADD KEY `role_id` (`role_id`);
-
---
--- Indexes for table `user_access`
---
-ALTER TABLE `user_access`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `menu_id` (`menu_id`),
-  ADD KEY `role_id` (`role_id`);
-
---
--- Indexes for table `user_menu`
---
-ALTER TABLE `user_menu`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `user_role`
---
-ALTER TABLE `user_role`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `user_submenu`
---
-ALTER TABLE `user_submenu`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `menu_id` (`menu_id`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `attendance`
---
-ALTER TABLE `attendance`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=72;
-
---
--- AUTO_INCREMENT for table `employee`
---
-ALTER TABLE `employee`
-  MODIFY `id` int(3) UNSIGNED ZEROFILL NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
-
---
--- AUTO_INCREMENT for table `employee_department`
---
-ALTER TABLE `employee_department`
-  MODIFY `id` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
-
---
--- AUTO_INCREMENT for table `location`
---
-ALTER TABLE `location`
-  MODIFY `id` int(1) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
-
---
--- AUTO_INCREMENT for table `shift`
---
-ALTER TABLE `shift`
-  MODIFY `id` int(1) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
-
---
--- AUTO_INCREMENT for table `user_access`
---
-ALTER TABLE `user_access`
-  MODIFY `id` int(2) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
-
---
--- AUTO_INCREMENT for table `user_menu`
---
-ALTER TABLE `user_menu`
-  MODIFY `id` int(2) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
-
---
--- AUTO_INCREMENT for table `user_role`
---
-ALTER TABLE `user_role`
-  MODIFY `id` int(1) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
-
---
--- AUTO_INCREMENT for table `user_submenu`
---
-ALTER TABLE `user_submenu`
-  MODIFY `id` int(2) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
-
---
 -- Constraints for dumped tables
 --
 
@@ -484,7 +325,7 @@ ALTER TABLE `attendance`
 -- Constraints for table `employee`
 --
 ALTER TABLE `employee`
-  ADD CONSTRAINT `shift_id_fk_e` FOREIGN KEY (`shift_id`) REFERENCES `shift` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION;
+  ADD CONSTRAINT `shift_id_fk_e` FOREIGN KEY (`shift_id`) REFERENCES `shift` (`id`) ON DELETE CASCADE;
 
 --
 -- Constraints for table `employee_department`
@@ -513,3 +354,7 @@ ALTER TABLE `user_access`
 ALTER TABLE `user_submenu`
   ADD CONSTRAINT `user_submenu_ibfk_1` FOREIGN KEY (`menu_id`) REFERENCES `user_menu` (`id`) ON UPDATE CASCADE;
 COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
